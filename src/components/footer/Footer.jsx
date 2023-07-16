bc805011a8c1c206acf95a1b2bb0d4e27acb00bd
simran.gajsinghani <simrangajsinhani259@gmail.com>
2023-07-16 19:11:37 +0530

    Added components and their styles

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div>©2023 Simsim. All rights reserved.</div>
+    </div>
+  );
+};
+
+export default Footer;
